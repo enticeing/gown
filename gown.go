33eac6b14e98ae58bd5a62c588a3ad6e82a8de3c
Christopher Mills <chris@redoubled.net>
2013-03-22 20:47:05 -0500

    First "working" code.

--- gown.go
+++ gown.go
@@ -6,9 +6,17 @@ import (
 )
 
 func main() {
-	conn, err := xgb.Dial(":0")
+	conn, err := xgb.Dial(":1")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(conn.Setup.Status)
+	width := conn.Setup.Roots[0].WidthInPixels
+	height := conn.Setup.Roots[0].HeightInPixels
+	fmt.Println(width, height)
+	
+	w := conn.NewId()
+	s := conn.DefaultScreen()
+	conn.CreateWindow(0, w, s.Root, 0, 0, 1920, 1080, 0, 0, 0, 0, nil)
+	conn.ChangeWindowAttributes(w, 0, []uint32{xgb.EventMaskSubstructureRedirect | xgb.EventMaskEnterWindow})
+	for {}
 }
