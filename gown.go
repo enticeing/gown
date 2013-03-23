9275858e4d7c04040bfcbfcce17a73260975746b
Christopher Mills <chris@redoubled.net>
2013-03-22 23:57:53 -0500

    Added keys.go

--- gown.go
+++ gown.go
@@ -3,28 +3,42 @@ package main
 import (
 	"code.google.com/p/x-go-binding/xgb"
 	"fmt"
+	"os/exec"
+	"os"
 )
 
-func main() {
+func main(){
 	conn, err := xgb.Dial(":1")
 	if err != nil {
 		panic(err)
 	}
-	wid := conn.Setup.Roots[0].WidthInPixels
-	hei := conn.Setup.Roots[0].HeightInPixels
-	fmt.Println(wid, hei)
 	
-	w := conn.NewId()
 	s := conn.DefaultScreen()
-	conn.CreateWindow(0, w, s.Root, 0, 0, wid, hei, 0, 0, 0, 0, nil)
-	conn.ChangeWindowAttributes(w, 0, []uint32{xgb.EventMaskSubstructureRedirect | xgb.EventMaskEnterWindow})
+
+	for _, v := range(Shortcuts) {
+		xgb.GrabKey(true,s.Root,v.Mod,v.Key,xgb.GrabModeAsync,xgb.GrabModeAsync)
+	}
+	
 	for{
 		event, _ := conn.WaitForEvent()
 		switch ev := event.(type) {
 		case xgb.MappingNotifyEvent:
 			fmt.Println("Mapping Notify Event:", ev)
-		default:
-			fmt.Println("Unkown event:", ev)
+		case xgb.KeyReleaseEvent:
+			switch ev.Detail {
+			case 67:
+				dmenu := exec.Command("dmenu_run")
+				dmenu.Start()
+			case 45:
+				os.Exit(0)
+			default:
+				fmt.Println("Key released:", ev.Detail)
+			}
 		}
 	}
+}
+
+func dmenu_run() {
+	dmenu := exec.Command("dmenu_run")
+	dmenu.Start()
 }
---
+++ keys.go
@@ -0,0 +1,51 @@
+package main
+
+import ("code.google.com/p/x-go-binding/xgb")
+// Because xgb is shitty and doesn't define key constants
+const (
+	K_a = 38
+	K_b = 56
+	K_c = 54
+	K_d = 40
+	K_e = 26
+	K_f = 41
+	K_g = 42
+	K_h = 43
+	K_i = 31
+	K_j = 44
+	K_k = 45
+	K_l = 46
+	K_m = 58
+	K_n = 57
+	K_o = 32
+	K_p = 33
+	K_q = 24
+	K_r = 27
+	K_s = 39
+	K_t = 28
+	K_u = 30
+	K_v = 55
+	K_w = 25
+	K_x = 53
+	K_y = 29
+	K_z = 52
+)
+
+// Shorter names for the modkeys
+const Mod4    = xgb.KeyButMaskMod4
+const Alt     = xgb KeyButMaskMod3
+const Shift   = xgb.KeyButMaskShift
+const Control = xgb.KeyButMaskControl
+
+type Shortcut struct {
+	Mod int
+	Key int
+	Function string
+}
+
+// Shortcuts with function names!
+// it's like magic!!
+
+const Shortcuts []Shortcut{
+	{Mod4, K_x, dmenu_run}
+}
