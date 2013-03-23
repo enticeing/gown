3ca99b5645107cd77a8a25622b0f7e994eb44a07
Christopher Mills <chris@redoubled.net>
2013-03-22 21:26:09 -0500

    Rendering windows now

--- gown.go
+++ gown.go
@@ -10,13 +10,21 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	width := conn.Setup.Roots[0].WidthInPixels
-	height := conn.Setup.Roots[0].HeightInPixels
-	fmt.Println(width, height)
+	wid := conn.Setup.Roots[0].WidthInPixels
+	hei := conn.Setup.Roots[0].HeightInPixels
+	fmt.Println(wid, hei)
 	
 	w := conn.NewId()
 	s := conn.DefaultScreen()
-	conn.CreateWindow(0, w, s.Root, 0, 0, 1920, 1080, 0, 0, 0, 0, nil)
+	conn.CreateWindow(0, w, s.Root, 0, 0, wid, hei, 0, 0, 0, 0, nil)
 	conn.ChangeWindowAttributes(w, 0, []uint32{xgb.EventMaskSubstructureRedirect | xgb.EventMaskEnterWindow})
-	for {}
+	for{
+		event, _ := conn.WaitForEvent()
+		switch ev := event.(type) {
+		case xgb.MappingNotifyEvent:
+			fmt.Println("Mapping Notify Event:", ev)
+		default:
+			fmt.Println("Unkown event:", ev)
+		}
+	}
 }
